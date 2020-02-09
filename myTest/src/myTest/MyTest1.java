88edd616f725b5b0a39db3584fefc1baa0d4188e
cuiyt-tony <itcyt123@126.com>
2020-02-09 23:07:36 +0800

    ssssssss

--- myTest/src/myTest/MyTest1.java
+++ myTest/src/myTest/MyTest1.java
@@ -3,7 +3,7 @@ package myTest;
 public class MyTest1 {
 
 	public static void main(String[] args) {
-		System.out.print("");
-		System.out.print("");
+		System.out.print("hello");
+		System.out.print("world");
 	}
 }
